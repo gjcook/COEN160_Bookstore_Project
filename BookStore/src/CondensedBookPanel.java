61fe9be5090dfa5f8f9baaf6fbe29df010f95974
Griffin Cook <gcook@scu.edu>
2016-05-17 22:53:16 -0700

    Making images work. note, you need an "image.jpg" for the time being as we have no images and no image paths listed with our test books.

--- BookStore/src/CondensedBookPanel.java
+++ BookStore/src/CondensedBookPanel.java
@@ -2,8 +2,12 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import javax.swing.BoxLayout;
 import java.awt.FlowLayout;
 import javax.swing.Box;
+import javax.swing.ImageIcon;
+import java.awt.Dimension;
 
 public class CondensedBookPanel extends JPanel {
 	private Book _book;
@@ -12,7 +16,7 @@ public class CondensedBookPanel extends JPanel {
 	private JLabel _author;
 	private JTextField _descriptionCondensed;
 	private JLabel _pages;
-	//private  _image;
+	private ImageIcon _image;
 	private JLabel _price;
 	
 	private JButton _view;
@@ -27,24 +31,33 @@ public class CondensedBookPanel extends JPanel {
 		_author = new JLabel(_book.getAuthor());
 		
 		_descriptionCondensed = new JTextField(_book.getDescription().substring(0, (_book.getDescription().length() < 40) ? _book.getDescription().length(): 40));
-		_pages = new JLabel(String.valueOf(_book.getPages()));
-		//_image = new JLabel(_book.getPathToImage());
-		_price = new JLabel(String.valueOf(_book.getPrice()));
+		_pages = new JLabel(String.valueOf(_book.getPages()) + " pages");
+		_price = new JLabel("$" + String.valueOf(_book.getPrice()));
 		
 		_view = new JButton("View Book");
 		_add = new JButton("Add to Cart");
+		
+		_image = new ImageIcon(new ImageIcon(System.getProperty("user.dir") + "//src//" +"image.jpg"/*_book.getImagePath()*/)
+				.getImage()
+				.getScaledInstance(60, 90, java.awt.Image.SCALE_SMOOTH));
+		
 		JPanel _middlePanel = new JPanel(), _eastPanel = new JPanel();
+		_middlePanel.setLayout(new FlowLayout());
 		_middlePanel.add(_title);
 		_middlePanel.add(_author);
 		_middlePanel.add(_descriptionCondensed);
 		_middlePanel.add(_pages);
 		
+		_eastPanel.setLayout(new BoxLayout(_eastPanel, BoxLayout.Y_AXIS));
 		_eastPanel.add(_price);
 		_eastPanel.add(_view);
 		_eastPanel.add(_add);
-		//add(_image, BorderLayout.WEST);
-		add(_middlePanel);//,BorderLayout.CENTER);
+		JLabel label = new JLabel(_image);
+		//label.setPreferredSize(new Dimension(100,150));
+		add(label, BorderLayout.WEST);
+		add(_middlePanel,BorderLayout.CENTER);
 		add(Box.createHorizontalGlue());
-		add(_eastPanel);//,BorderLayout.EAST);
+		add(_eastPanel,BorderLayout.EAST);
+		
 	}
 }
